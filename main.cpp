ad4be8160e33ed2e68c1a6b4ae7bb554f79e4e10
Fukurou-chan <47669603+Fukurou-chan@users.noreply.github.com>
2019-12-26 13:52:41 +0700

    Just main

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "tree.hpp"
+#include "avl.hpp"
+#include "workfile.hpp"
+#include "welcome.hpp"
+using namespace std;
+int main()
+{
+    Welcome wel;
+    wel.dictionary();       
+	  return 0;
+}
